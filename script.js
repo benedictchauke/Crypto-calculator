5a9ffdbd76091f3689341487ae826a491b040358
Benedict Isaac Chauke <benedictchauke@hotmail.com>
2021-08-04 04:10:40 +0200

    adding values and api

--- script.js
+++ script.js
@@ -8,7 +8,19 @@ const swap = document.getElementById('swap');
 
 //Fetch exchange rates and update the DOM
 function compute() {
-    console.log('test');
+    const c_one = cEl_one.value;
+    const c_two = cEl_two.value;
+
+    fetch('https://api.exchangerate-api.com/v4/latest/${c_one}')
+    .then(res => res.json())
+    .then(data => {
+        const rate = data.rates[c_two];
+
+        rEl.innerText = '1 ${c_one} = ${rate} ${c_two}';
+
+        aEl_two.value = (aEl_one.value * rate).toFixed(2);
+    });
+
 }
 
 //Event listeners
@@ -17,4 +29,10 @@ aEl_one.addEventListener('input', compute);
 cEl_two.addEventListener('change', compute);
 aEl_two.addEventListener('input', compute);
 
+swap.addEventListener('click', ()=> {
+    const temp = cEl_one.value;
+    cEl_one.value = cEl_two.value;
+    cEl_two = temp;
+    compute();
+} );
 compute();
